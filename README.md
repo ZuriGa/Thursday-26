051e28f09e8ae14fcfc668a7ea2aaa1b06e7a84a
Zuri <zurisadailopezmartinez@gmail.com>
2023-10-26 11:35:23 -0700

    WIP
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+
+
+
+
+
+Test: Make sure input are string of numbers code:  
+Expected output: string with one of two messages, display a message whether the card a valid or not 
+
+Test: Make sure card number us 16 digits Code:  Expected output: to determine if a card number is valid based on its length.
+
+
+
+
+
+
+
+Test: make sure its a validated card, AE starts with 34/37, Visa starts with 4,  Mastercard starts with 5, Discover starts with 6. Code: Expected output: to determine whether the credit card number was issued by an accredited company.
+
+
+Test: Double every other digit from right to left, if result is double digits add the 2 digits together code:  Expected output:
+
+
+Test: sum all digits in new value, if sum does end with a 0, its valid, if does not end with 0 is not valid 
+
+
+
---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+//Business
+
+// function validateCreditCard(cardNum) {
+//     let creditCardInt = cardNum.split(' ').map(number);
+//     for (let i=creditCardInt.length -2; i >=0; i= 1-2) {
+//         let tempValue = creditCardInt[i];
+//         tempValue = tempValue * 2;
+//         if (tempValue > 9) {
+//             tempValue = tempValue % 10 + 1;
+//         }
+//     }
+
+// }
+
+
+function validateCreditCardMasterCard(cardNum){
+
+const tempCardNum = cardNum
+tempcardNum.slice(0,2 )
+if tempCardNum = "5"{
+
+}
+
+}
+
+function validateCreditCardVisa(cardNum){
+
+
+
+}
+function validateCreditCardDiscover(cardNum){
+
+}
+function validateCreditCardAE(cardNum){
+
+
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//UI 
+function handleFormSubmission(e) {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    const cardNum = document.getElementById("cardNum");
+
+
+}
+
+
+
+window.addEventListener("load", function() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", handleFormSubmission);
+});
+
+
