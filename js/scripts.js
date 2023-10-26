44f44ee7c00432328b7635b71a654c86204c8d5c
Zuri <zurisadailopezmartinez@gmail.com>
2023-10-26 11:51:02 -0700

    WIP Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,16 @@
-//Business
+// //Business
 
-// function validateCreditCard(cardNum) {
-//     let creditCardInt = cardNum.split(' ').map(number);
-//     for (let i=creditCardInt.length -2; i >=0; i= 1-2) {
-//         let tempValue = creditCardInt[i];
-//         tempValue = tempValue * 2;
-//         if (tempValue > 9) {
-//             tempValue = tempValue % 10 + 1;
-//         }
-//     }
+function stringToArray(cardNum){
 
-// }
-
-
-function validateCreditCardMasterCard(cardNum){
-
-const tempCardNum = cardNum
-tempcardNum.slice(0,2 )
-if tempCardNum = "5"{
-
+    cardArray = cardNum.split(" ")
+    return reverseArray;
 }
+function reverseArray(cardArray){
 
+
+    cardArray.reverse()
+    return 
 }
-
-function validateCreditCardVisa(cardNum){
-
-
-
-}
-function validateCreditCardDiscover(cardNum){
-
-}
-function validateCreditCardAE(cardNum){
-
-
-
-}
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //UI 
@@ -85,3 +30,41 @@ window.addEventListener("load", function() {
 });
 
 
+
+// // function validateCreditCard(cardNum) {
+// //     let creditCardInt = cardNum.split(' ').map(number);
+// //     for (let i=creditCardInt.length -2; i >=0; i= 1-2) {
+// //         let tempValue = creditCardInt[i];
+// //         tempValue = tempValue * 2;
+// //         if (tempValue > 9) {
+// //             tempValue = tempValue % 10 + 1;
+// //         }
+// //     }
+
+// // }
+
+
+// function validateCreditCardMasterCard(cardNum){
+
+// const tempCardNum = cardNum
+// tempcardNum.slice(0,2 )
+// if tempCardNum = "5"{
+
+// }
+
+// }
+
+// function validateCreditCardVisa(cardNum){
+
+
+
+// }
+// function validateCreditCardDiscover(cardNum){
+
+// }
+// function validateCreditCardAE(cardNum){
+
+
+
+// }
+    
